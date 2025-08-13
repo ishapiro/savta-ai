e500879cb3d57e82d5c1d29f13cb38a3518d800f
Irv Shapiro <ishapiro@cogitations.com>
2025-08-13 11:13:37 -0500

    Updates to webhooks for sendmail

--- server/api/webhooks/sendgrid.get.js
+++ server/api/webhooks/sendgrid.get.js
@@ -1,8 +1,1 @@
-export default defineEventHandler(async (event) => {
-  return {
-    success: true,
-    message: 'SendGrid webhook endpoint is accessible',
-    method: 'GET',
-    timestamp: new Date().toISOString()
-  }
-})
+ 
--- server/api/webhooks/test.get.js
+++ server/api/webhooks/test.get.js
@@ -1,7 +1,1 @@
-export default defineEventHandler(async (event) => {
-  return {
-    success: true,
-    message: 'Webhooks directory is accessible',
-    timestamp: new Date().toISOString()
-  }
-})
+ 
